246b711d99147fb395420b6205bfe2f9a01d15db
VladGTest <vladislavfordev@gmail.com>
2025-09-13 16:46:10 +0300

    Find Most Frequent Vowel and Consonant

---
+++ problems/find-most-frequent-vowel-and-consonant/solution.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def maxFreqSum(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        vowels = "aeiou"
+        vowel_freq = {}
+        consonant_freq = {}
+        
+        for char in s:
+            if char in vowels:
+                vowel_freq[char] = vowel_freq.get(char, 0) + 1
+            else:
+                consonant_freq[char] = consonant_freq.get(char, 0) + 1
+        
+        max_vowel = max(vowel_freq.values()) if vowel_freq else 0
+        max_consonant = max(consonant_freq.values()) if consonant_freq else 0
+        
+        return max_vowel + max_consonant
