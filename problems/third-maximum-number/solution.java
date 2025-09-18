8968395acc78e0cc0e2fd3e5c37d1be2f85ddbd6
VladGTest <vladislavfordev@gmail.com>
2025-09-18 11:25:18 +0300

    Third Maximum Number

---
+++ problems/third-maximum-number/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+        long firstMax = Long.MIN_VALUE;
+        long secondMax = Long.MIN_VALUE;
+        long thirdMax = Long.MIN_VALUE;
+
+        for (int num : nums) {
+            if (num == firstMax || num == secondMax || num == thirdMax) {
+                continue;
+            }
+            if (num > firstMax) {
+                thirdMax = secondMax;
+                secondMax = firstMax;
+                firstMax = num;
+            } else if (num > secondMax) {
+                thirdMax = secondMax;
+                secondMax = num;
+            } else if (num > thirdMax) {
+                thirdMax = num;
+            }
+        }
+        return (int) (thirdMax != Long.MIN_VALUE ? thirdMax : firstMax);
+    }
+}
