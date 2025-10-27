f55d806a7b88500ed194dcdecffbdf2f685b5fbe
VladGTest <vladislavfordev@gmail.com>
2025-10-27 18:26:23 +0300

    Number of Laser Beams in a Bank

---
+++ problems/number-of-laser-beams-in-a-bank/solution.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def numberOfBeams(self, bank):
+        """
+        :type bank: List[str]
+        :rtype: int
+        """
+        total_beams = 0
+        prev_devices = 0
+        for row in bank:
+            curr_devices = row.count("1")
+            if curr_devices > 0:
+                total_beams += prev_devices * curr_devices
+                prev_devices = curr_devices
+        return total_beams
+        
