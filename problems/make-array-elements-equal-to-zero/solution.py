2458974c0fe2a5b3eeb1004616d0824b4169ca45
VladGTest <vladislavfordev@gmail.com>
2025-10-28 18:51:35 +0300

    Make Array Elements Equal to Zero

---
+++ problems/make-array-elements-equal-to-zero/solution.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def countValidSelections(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        total_sum = sum(nums)
+        valid_count = 0
+        left_sum = 0
+        for num in nums:
+            if num != 0:
+                left_sum += num
+            else:
+                if left_sum * 2 == total_sum:
+                    valid_count += 2
+                elif abs(left_sum * 2 - total_sum) == 1:
+
+                    valid_count += 1
+        return valid_count
+        
